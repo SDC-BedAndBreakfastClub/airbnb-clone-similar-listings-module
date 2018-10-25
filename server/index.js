b8193a7907f0e99545462d7bb9d05940b2bdabf3
Eric Callari <ericcallari1@gmail.com>
2018-10-25 16:02:43 -0700

    serving static files

--- server/index.js
+++ server/index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
 const port = 3003;
+const bodyParser = require('body-parser');
+const path = require('path');
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use('/', express.static(path.join(__dirname, 'client/div')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'client', 'dist', 'index.html'));
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
