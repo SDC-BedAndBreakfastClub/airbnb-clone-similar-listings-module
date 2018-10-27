eb6fb01baf2e8e6f22d5ebfe596c58842bf36bbd
Eric Callari <ericcallari1@gmail.com>
2018-10-26 17:21:18 -0700

    made changes based on feedback

--- server/index.js
+++ server/index.js
@@ -7,8 +7,4 @@ const port = 3003;
 
 app.use('/', express.static('public'));
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.resolve(__dirname, '..', 'public', 'index.html'));
-// });
-
 app.listen(port, () => console.log(`Listening on port ${port}`));
