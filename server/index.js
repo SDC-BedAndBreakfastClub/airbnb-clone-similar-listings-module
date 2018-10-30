00f0b5f996e0dcc70fda295db5c0c4d8155920c9
Eric Callari <ericcallari1@gmail.com>
2018-10-29 20:06:26 -0700

    made requested changes

--- server/index.js
+++ server/index.js
@@ -2,14 +2,7 @@ const express = require('express');
 
 const app = express();
 const port = 3003;
-const db = require('../data/index.js');
-// const path = require('path');
-// const bodyParser = require('body-parser');
 
 app.use('/', express.static('public'));
-
-app.get('/listings', (req, res) => {
-  db.getAll();
-});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
