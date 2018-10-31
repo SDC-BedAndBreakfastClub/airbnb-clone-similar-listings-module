fc6566380cc28cef282f847d8714c2dc24557be6
Eric Callari <ericcallari1@gmail.com>
2018-10-30 20:12:06 -0700

    alter endpoint

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use('/', express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/api/rooms/', (req, res) => {
+app.get('/api/rooms/:roomId/similar_listings', (req, res) => {
   model.get12((err, results) => {
     if (err) {
       throw err;
