91718560512e7c79f5bb96a5685d1f606ba54381
Eric Callari <ericcallari1@gmail.com>
2018-11-01 22:53:40 -0700

    buttons relocated and styled

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -48,29 +48,47 @@ class App extends React.Component {
       transform: `translateX(${displacement}%`,
       transition: '-webkit-transform 0.5s ease-in-out',
     };
+    const buttonStyle = {
+      height: '24px',
+      width: '24px',
+      display: 'block',
+      fill: 'rgb(118, 118, 118)',
+    };
     if (listings.length) {
       return (
-        <div>
+        <div className="center-page">
           <h2 className="main-title">Similar Listings</h2>
-          <span>
+          {/* <span>
             <button type="button" onClick={() => this.slideRight()}>LAST</button>
             <button type="button" onClick={() => this.slideLeft()}>NEXT</button>
-          </span>
-          <div className="wrap">
-            <div className="carousel" style={cssStyle}>
-              <Listing listing={listings[0]} />
-              <Listing listing={listings[1]} />
-              <Listing listing={listings[2]} />
-              <Listing listing={listings[3]} />
-              <Listing listing={listings[4]} />
-              <Listing listing={listings[5]} />
-              <Listing listing={listings[6]} />
-              <Listing listing={listings[7]} />
-              <Listing listing={listings[8]} />
-              <Listing listing={listings[9]} />
-              <Listing listing={listings[10]} />
-              <Listing listing={listings[11]} />
+          </span> */}
+          <div className="outermost">
+            <button className="prev-arrow" type="button" onClick={() => this.slideRight()}>
+              <svg viewBox="0 0 18 18" role="img" aria-label="Previous" focusable="false" style={buttonStyle}>
+                <path d="m13.7 16.29a1 1 0 1 1 -1.42 1.41l-8-8a1 1 0 0 1 0-1.41l8-8a1 1 0 1 1 1.42 1.41l-7.29 7.29z" fillRule="evenodd" />
+              </svg>
+            </button>
+            <div className="wrap">
+              <div className="carousel" style={cssStyle}>
+                <Listing listing={listings[0]} />
+                <Listing listing={listings[1]} />
+                <Listing listing={listings[2]} />
+                <Listing listing={listings[3]} />
+                <Listing listing={listings[4]} />
+                <Listing listing={listings[5]} />
+                <Listing listing={listings[6]} />
+                <Listing listing={listings[7]} />
+                <Listing listing={listings[8]} />
+                <Listing listing={listings[9]} />
+                <Listing listing={listings[10]} />
+                <Listing listing={listings[11]} />
+              </div>
             </div>
+            <button className="next-arrow" type="button" onClick={() => this.slideLeft()}>
+              <svg viewBox="0 0 18 18" role="img" aria-label="Previous" focusable="false" style={buttonStyle}>
+                <path d="m4.29 1.71a1 1 0 1 1 1.42-1.41l8 8a1 1 0 0 1 0 1.41l-8 8a1 1 0 1 1 -1.42-1.41l7.29-7.29z" fillRule="evenodd" />
+              </svg>
+            </button>
           </div>
         </div>
       );
