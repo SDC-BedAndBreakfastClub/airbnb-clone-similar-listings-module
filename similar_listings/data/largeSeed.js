6139296b8dc703cc993d7becd52607d52a2d1546
Keiran <keirans@gmail.com>
2018-11-16 10:53:55 -0800

    exclude csv output from .git

--- similar_listings/data/largeSeed.js
+++ similar_listings/data/largeSeed.js
@@ -4,7 +4,8 @@ const fake = require('faker');
 const HipsterIpsum = require('hipsteripsum');
 const _ = require('underscore');
 
-const filePath = path.resolve(__dirname, 'data.json');
+const jsonFile = path.resolve(__dirname, 'data.json');
+const csvFile = path.resolve(__dirname, 'data.csv');
 
 const hipIp = (numOfWords) => {
   let words = HipsterIpsum.get();
@@ -29,39 +30,70 @@ const similarListings = (num) => {
   return output;
 };
 
-const generateRecord = (idx) => {
-  const oneListing = {
-    id: idx,
-    images: choosePhotoBin(),
-    type: 'ENTIRE HOME',
-    beds: `${_.random(2, 4)} BEDS`,
-    title: `${hipIp(2)} in ${fake.address.county()}`,
-    price: _.random(39, 249),
-    ratings: _.random(40, 270),
-    average_rating: _.random(3, 5),
-    similar: similarListings(12),
-  };
-  return JSON.stringify(oneListing);
-};
-
-const seedData = fs.createWriteStream(filePath);
-let record = 1;
-
-const write = () => {
-  const startTime = Date.now();
-  let ok = true;
-  while (record < 10000001 && ok) {
-    ok = seedData.write(generateRecord(record));
+let listings = '';
+for (let chunk = 0; chunk < 200; chunk += 1) {
+  for (let i = 1; i < 50001; i += 1) {
+    listings += (i + (chunk * 100000));
+    listings += ',';
+    listings += JSON.stringify(choosePhotoBin());
+    listings += ',';
+    listings += 'ENTIRE HOME';
+    listings += ',';
+    listings += `${_.random(2, 4)} BEDS`;
+    listings += ',';
+    listings += `${hipIp(2)} in ${fake.address.county()}`;
+    listings += ',';
+    listings += _.random(39, 249);
+    listings += ',';
+    listings += _.random(40, 270);
+    listings += ',';
+    listings += _.random(3, 5);
+    listings += '\n';
     console.clear();
-    console.log(`${record} records written`);
-    record += 1;
+    console.log(`${i + (chunk * 100000)} records generated`);
   }
-  if (record < 10000001) {
-    seedData.once('drain', write);
-  } else {
-    seedData.end();
-    const endTime = Date.now();
-    console.log(`${record - 1} records written to file in ${endTime - startTime}ms`);
-  }
-};
-write();
+  fs.writeFileSync(csvFile, listings, { flag: 'a' }, (err) => { // write CSV
+    if (err) {
+      console.log(err);
+    }
+  });
+}
+
+//=======================================JSON DATA GENERATION=============================================
+// const generateRecord = (idx) => {
+//   const oneListing = {
+//     id: idx,
+//     images: choosePhotoBin(),
+//     type: 'ENTIRE HOME',
+//     beds: `${_.random(2, 4)} BEDS`,
+//     title: `${hipIp(2)} in ${fake.address.county()}`,
+//     price: _.random(39, 249),
+//     ratings: _.random(40, 270),
+//     average_rating: _.random(3, 5),
+//     similar: similarListings(12),
+//   };
+//   return JSON.stringify(oneListing);
+// };
+
+// const seedData = fs.createWriteStream(jsonFile);
+// let record = 1;
+
+// const write = () => {
+//   const startTime = Date.now();
+//   let ok = true;
+//   while (record < 10000001 && ok) {
+//     ok = seedData.write(generateRecord(record));
+//     console.clear();
+//     console.log(`${record} records written`);
+//     record += 1;
+//   }
+//   if (record < 10000001) {
+//     seedData.once('drain', write);
+//   } else {
+//     seedData.end();
+//     const endTime = Date.now();
+//     console.log(`${record - 1} records written to file in ${endTime - startTime}ms`);
+//   }
+// }
+
+// write();
