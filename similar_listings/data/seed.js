408a5f489a6155d95e5ade1d5f47c26f0972981f
Keiran <keirans@gmail.com>
2018-11-15 20:02:07 -0800

    trim further unused information from schema

--- similar_listings/data/seed.js
+++ similar_listings/data/seed.js
@@ -11,7 +11,6 @@ mongoose.connect('mongodb://database/listing', (err) => {
 const listingSchema = new mongoose.Schema({
   id: Number,
   images: [String],
-  saved: Boolean,
   type: String,
   beds: String,
   title: String,
@@ -43,7 +42,6 @@ for (let i = 1; i < 101; i += 1) {
   const oneListing = {
     id: i,
     images: choosePhotoBin(),
-    saved: 0,
     type: 'ENTIRE HOME',
     beds: `${_.random(2, 4)} BEDS`,
     title: `${hipIp(2)} in ${fake.address.county()}`,
