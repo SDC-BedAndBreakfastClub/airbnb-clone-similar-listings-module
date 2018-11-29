4a0a9a60a21f6723efd1ea4c73b0c6cd0c41964b
Keiran <keirans@gmail.com>
2018-11-29 11:09:36 -0800

    refactor server to respond to the same url path as the proxy

--- similar_listings/server/index.js
+++ similar_listings/server/index.js
@@ -7,10 +7,22 @@ const connection = require('../data/index');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use('/', express.static(path.resolve(__dirname, '../public')));
+app.use(express.static(path.resolve(__dirname, '../public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
+
+app.get('/rooms/:listingId', (req, res) => {
+  const filePath = path.resolve(__dirname, '../public/index.html');
+  res.sendFile(filePath, (err) => {
+    if (err) {
+      console.log(err);
+      res.sendStatus(500);
+    } else {
+      console.log('index file sent');
+    }
+  });
+});
 
 app.get('/api/rooms/:listingId/similar_listings', (req, res) => {
   const { listingId } = req.params;
@@ -35,7 +47,7 @@ app.post('/api/rooms/:listingId/similar_listings', (req, res) => {
   });
 });
 
-app.patch('/api/rooms/:listingId', (req, res) => {
+app.patch('/api/rooms/:listingId/similar_listings', (req, res) => {
   const { listingId } = req.params;
   const changes = req.body;
   connection.editListing(listingId, changes, (err, updated) => {
